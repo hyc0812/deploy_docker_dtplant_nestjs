69296d3fe50dd5b17b853281b226a544f6331a37
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-11-05 16:34:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Learning NestJS... 
+
+The first practice project. 
---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+
+@Module({
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nestjs',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    ProfileModule,
+  
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
